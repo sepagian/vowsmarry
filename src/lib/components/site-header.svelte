1c4465fed1e8fcc696321321046ea3e2b8e73eeb
sepagian <rizkydana14@gmail.com>
2025-08-27 22:36:29 +0800

    feat(header): update site header component for improved wedding planner branding

--- src/lib/components/site-header.svelte
+++ src/lib/components/site-header.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
 	import * as Sidebar from '$lib/components/ui/sidebar/index';
 	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index';
+	import { page } from '$app/state';
 	import { Button } from '$lib/components/ui/button/index';
 	import { Separator } from '$lib/components/ui/separator/index';
 	import { PanelRight } from 'lucide-svelte';
 	const sidebar = Sidebar.useSidebar();
+
+	const breadcrumbs = $derived(
+		page.url.pathname
+			.split('/')
+			.filter(Boolean) // Remove empty segments from leading/trailing slashes
+			.map((segment, index, arr) => {
+				const href = '/' + arr.slice(0, index + 1).join('/');
+				// Capitalize the segment for display
+				const title = segment.replace(/-/g, ' ').replace(/\b\w/g, (c) => c.toUpperCase());
+				return { title, href, isLast: index === arr.length - 1 };
+			})
+	);
 </script>
 
 <header class="bg-background sticky top-0 z-50 flex w-full items-center border-b">
@@ -15,13 +28,18 @@
 		<Separator orientation="vertical" class="mr-2 h-4" />
 		<Breadcrumb.Root class="hidden sm:block">
 			<Breadcrumb.List>
-				<Breadcrumb.Item>
-					<Breadcrumb.Link href="#">Building Your Application</Breadcrumb.Link>
-				</Breadcrumb.Item>
-				<Breadcrumb.Separator />
-				<Breadcrumb.Item>
-					<Breadcrumb.Page>Data Fetching</Breadcrumb.Page>
-				</Breadcrumb.Item>
+				{#each breadcrumbs as crumb, i (i)}
+					<Breadcrumb.Item>
+						{#if crumb.isLast}
+							<Breadcrumb.Page>{crumb.title}</Breadcrumb.Page>
+						{:else}
+							<Breadcrumb.Link href={crumb.href}>{crumb.title}</Breadcrumb.Link>
+						{/if}
+					</Breadcrumb.Item>
+					{#if !crumb.isLast}
+						<Breadcrumb.Separator />
+					{/if}
+				{/each}
 			</Breadcrumb.List>
 		</Breadcrumb.Root>
 	</div>
