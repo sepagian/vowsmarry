26015635d409ccd4c5dffe86be82c50760921f50
sepagian <rizkydana14@gmail.com>
2025-08-27 11:39:39 +0800

    feat(ui): add shadcn-svelte

---
+++ uno.config.ts
@@ -0,0 +1,40 @@
+import {
+	defineConfig,
+	presetAttributify,
+	presetIcons,
+	presetTypography,
+	presetWebFonts,
+	presetWind4,
+	transformerDirectives,
+	transformerVariantGroup,
+	transformerCompileClass
+} from 'unocss';
+import presetAnimations from 'unocss-preset-animations';
+import { builtinColors, presetShadcn } from 'unocss-preset-shadcn';
+
+export default defineConfig({
+	presets: [
+		presetWind4(),
+		presetAttributify(),
+		presetIcons({
+			scale: 1.2,
+			warn: true
+		}),
+		presetTypography(),
+		presetWebFonts({
+			provider: 'google', // default provider
+			fonts: {
+				sans: 'Rethink Sans',
+				serif: 'Instrument Serif',
+				mono: 'Fira Code',
+				body: 'Open Sans'
+			}
+		}),
+		presetAnimations(),
+		presetShadcn(builtinColors.map((c) => ({ color: c })))
+	],
+	transformers: [transformerDirectives(), transformerVariantGroup(), transformerCompileClass()],
+	safelist: [
+		// Add any classes that should always be included in the output here
+	]
+});
