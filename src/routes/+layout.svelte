427e22d8ee5074bfe06834480526f8689ab799b4
sepagian <rizkydana14@gmail.com>
2025-08-27 16:19:31 +0800

    feat(routes): added public routes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,28 @@
 <script lang="ts">
+	import * as Sidebar from '$lib/components/ui/sidebar/index';
+	import AppSidebar from '$lib/components/app-sidebar.svelte';
+	import SiteHeader from '$lib/components/site-header.svelte';
 	import favicon from '$lib/assets/favicon.svg';
+	import 'uno.css';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>vowsmarry</title>
 </svelte:head>
 
-{@render children?.()}
+<div class="[--header-height:calc(--spacing(14))]">
+	<Sidebar.Provider class="flex flex-col">
+		<SiteHeader />
+		<div class="flex flex-1">
+			<AppSidebar />
+			<Sidebar.Inset>
+				<main>
+					{@render children()}
+				</main>
+			</Sidebar.Inset>
+		</div>
+	</Sidebar.Provider>
+</div>
