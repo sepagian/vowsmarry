3b9722bd9b4025a8955bc0787d5fd724e7ffb715
sepagian <rizkydana14@gmail.com>
2025-08-28 11:14:27 +0800

    feat(auth): add login layout structure

---
+++ src/routes/(auth)/login/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import 'uno.css';
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>Login to vowsmarry</title>
+</svelte:head>
+
+<div class="flex h-screen w-full items-center justify-center px-4">
+	{@render children()}
+</div>
