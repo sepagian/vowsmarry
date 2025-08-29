fd75a06927f89459d5898a941ad3eadacdaaa7c9
sepagian <rizkydana14@gmail.com>
2025-08-29 22:48:14 +0800

    refactor(auth): simplify logout logic and update redirect handling

--- src/routes/(auth)/logout/+page.server.ts
+++ src/routes/(auth)/logout/+page.server.ts
@@ -1,17 +1,13 @@
-import { redirect } from '@sveltejs/kit';
-import { invalidateSession, deleteSessionTokenCookie } from '$lib/server/auth';
-import type { Actions, PageServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit'
+import type { Actions, PageServerLoad } from './$types'
 
 export const load: PageServerLoad = async () => {
-	throw redirect(302, '/login');
-};
+	redirect(302, '/login')
+}
 
 export const actions: Actions = {
-	default: async ({ locals, cookies }) => {
-		if (locals.session) {
-			await invalidateSession(locals.session.id);
-		}
-		deleteSessionTokenCookie({ cookies } as any);
-		throw redirect(302, '/login');
+	default: async ({ locals: { supabase } }) => {
+		await supabase.auth.signOut()
+		redirect(302, '/login')
 	}
-};
+}
