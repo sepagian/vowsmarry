daa5e0355181d6287432978b6c51b2ccdb5d6aa1
sepagian <rizkydana14@gmail.com>
2025-08-29 22:45:47 +0800

    feat: add client-side supabase configuration

---
+++ src/routes/+layout.ts
@@ -0,0 +1,37 @@
+import { createBrowserClient, createServerClient, isBrowser } from '@supabase/ssr'
+import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
+import type { LayoutLoad } from './$types'
+
+export const load: LayoutLoad = async ({ data, depends, fetch }) => {
+	/**
+	 * Declare a dependency so the layout can be invalidated, for example, on
+	 * session refresh.
+	 */
+	depends('supabase:auth')
+
+	const supabase = isBrowser()
+		? createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+				global: {
+					fetch,
+				},
+		  })
+		: createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+				global: {
+					fetch,
+				},
+				cookies: {
+					getAll() {
+						return data.cookies
+					},
+				},
+		  })
+
+	/**
+	 * Use the user data from the server which was validated using getUser().
+	 * Session is set to null since it's not needed in client-side code.
+	 */
+	const session = data.session
+	const user = data.user
+
+	return { session, supabase, user }
+}
