c5e352f80644b2ca2efe7f23cd6a8c9b81e472b0
sepagian <rizkydana14@gmail.com>
2025-08-29 22:32:34 +0800

    feat: update app types for supabase auth integration

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,15 +1,21 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+import type { Session, SupabaseClient, User } from '@supabase/supabase-js'
+
 declare global {
 	namespace App {
 		interface Locals {
-			user: import('$lib/server/auth').SessionValidationResult['user'];
-			session: import('$lib/server/auth').SessionValidationResult['session'];
+			supabase: SupabaseClient
+			safeGetSession: () => Promise<{ session: Session | null; user: User | null }>
+			session: Session | null
+			user: User | null
 		}
-	} // interface Error {}
-	// interface Locals {}
-} // interface PageData {}
-// interface PageState {}
+		interface PageData {
+			session: Session | null
+		}
+		// interface Error {}
+		// interface Platform {}
+	}
+}
 
-// interface Platform {}
 export {};
