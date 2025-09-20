464e461586b56808de443edf12427caed0152363
sepagian <rizkydana14@gmail.com>
2025-09-20 16:33:45 +0800

    feat: update types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,3 +4,23 @@ export type Option<T> = {
 	icon?: string;
 	color?: string;
 };
+
+export type Category =
+	| 'accommodation'
+	| 'catering'
+	| 'decoration'
+	| 'entertainment'
+	| 'makeup-attire'
+	| 'paperwork'
+	| 'photo-video'
+	| 'venue'
+	| 'miscellaneous';
+
+export type OverviewCard = {
+	title: string;
+	description: string;
+	action?: string;
+	actionClass?: string;
+	actionColor?: string;
+	footer: string;
+};
