58ff412c27df119e2b3a738b87bba396ce0d0f34
sepagian <rizkydana14@gmail.com>
2025-09-20 16:33:44 +0800

    feat: add vendor section

--- src/lib/components/section/section-vendor.svelte
+++ src/lib/components/section/section-vendor.svelte
@@ -5,46 +5,23 @@
 	import { Button, buttonVariants } from '../ui/button/index';
 	import Board from '../dnd/board.svelte';
 	import DialogVendor from '../dialog/dialog-vendor.svelte';
+	import { vendorsColumns, type ColumnType, type Item } from '$lib/stores/vendors';
 
-	let columnsData: ColumnType[] = [
-		{
-			id: 'c1',
-			name: 'Booked',
-			items: [
-				{ id: '1', name: 'item41' },
-				{ id: '2', name: 'item42' },
-				{ id: '3', name: 'item43' },
-				{ id: '4', name: 'item44' },
-				{ id: '5', name: 'item45' },
-				{ id: '6', name: 'item46' },
-				{ id: '7', name: 'item47' },
-				{ id: '8', name: 'item48' },
-				{ id: '9', name: 'item49' },
-			],
-		},
-		{
-			id: 'c2',
-			name: 'Contacted',
-			items: [
-				{ id: '10', name: 'item50' },
-				{ id: '11', name: 'item51' },
-			],
-		},
-		{
-			id: 'c3',
-			name: 'Negotiated',
-			items: [{ id: '13', name: 'item52' }],
-		},
-		{
-			id: 'c4',
-			name: 'Completed',
-			items: [{ id: '14', name: 'item53' }],
-		},
-	];
+	let columnsData: ColumnType[];
+
+	// Subscribe to the store
+	const unsubscribe = vendorsColumns.subscribe((value) => {
+		columnsData = value;
+	});
+
 	function handleBoardUpdated(newColumnsData: ColumnType[]) {
-		// if you wanted to update a database or a server, this is where you would do it
-		columnsData = newColumnsData;
+		// Update the store
+		vendorsColumns.set(newColumnsData);
 	}
+
+	// Cleanup on destroy
+	import { onDestroy } from 'svelte';
+	onDestroy(unsubscribe);
 </script>
 
 <div class="flex flex-col gap-4 px-4">
