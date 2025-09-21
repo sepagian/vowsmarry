d0b85052d3ba9ae396528906e829b34c23920e29
sepagian <rizkydana14@gmail.com>
2025-09-21 22:14:12 +0800

    refactor: update vendor page to use new section

--- src/routes/dashboard/vendor/+page.svelte
+++ src/routes/dashboard/vendor/+page.svelte
@@ -1,42 +1,49 @@
 <script lang="ts">
 	import SectionCards from '$lib/components/section/section-cards.svelte';
 	import SectionVendor from '$lib/components/section/section-vendor.svelte';
-	import { vendorsColumns } from '$lib/stores/vendors';
-	import type { ColumnType } from '$lib/stores/vendors';
+	import { vendorsStore } from '$lib/stores/vendors';
 
 	const overviewTitle = 'Vendors Overview';
 
 	// Reactive overviewCards based on the store
-	$: overviewCards = $vendorsColumns.map((column: ColumnType) => {
-		const count = column.items.length;
-		let actionClass = '';
-		let actionColor = '';
-		switch (column.name) {
-			case 'Booked':
-				actionClass = 'i-lucide:badge-check';
-				actionColor = 'bg-green-500 text-white';
-				break;
-			case 'Contacted':
-				actionClass = 'i-lucide:badge-minus';
-				actionColor = 'bg-yellow-500 text-white';
-				break;
-			case 'Negotiated':
-				actionClass = 'i-lucide:badge-alert';
-				actionColor = 'bg-red-500 text-white';
-				break;
-			case 'Completed':
-				actionClass = 'i-lucide:badge-info';
-				actionColor = 'bg-blue-500 text-white';
-				break;
-		}
-		return {
-			title: count.toString(),
-			description: column.name,
-			actionClass,
-			actionColor,
-			footer: `Updated just now`,
-		};
-	});
+	$: overviewCards = (() => {
+		const vendors = $vendorsStore;
+		const researching = vendors.filter((vendor) => vendor.vendorStatus === 'researching').length;
+		const contacted = vendors.filter((vendor) => vendor.vendorStatus === 'contacted').length;
+		const quoted = vendors.filter((vendor) => vendor.vendorStatus === 'quoted').length;
+		const booked = vendors.filter((vendor) => vendor.vendorStatus === 'booked').length;
+
+		return [
+			{
+				title: researching.toString(),
+				description: 'Researching',
+				actionClass: 'i-lucide:search',
+				actionColor: 'bg-gray-500 text-white',
+				footer: 'Updated just now',
+			},
+			{
+				title: contacted.toString(),
+				description: 'Contacted',
+				actionClass: 'i-lucide:phone',
+				actionColor: 'bg-yellow-500 text-white',
+				footer: 'Updated just now',
+			},
+			{
+				title: quoted.toString(),
+				description: 'Quoted',
+				actionClass: 'i-lucide:message-square-quote',
+				actionColor: 'bg-blue-500 text-white',
+				footer: 'Updated just now',
+			},
+			{
+				title: booked.toString(),
+				description: 'Booked',
+				actionClass: 'i-lucide:book-check',
+				actionColor: 'bg-green-500 text-white',
+				footer: 'Updated just now',
+			},
+		];
+	})();
 </script>
 
 <div class="flex flex-1 flex-col gap-4 py-4 max-w-screen-xl mx-auto">
