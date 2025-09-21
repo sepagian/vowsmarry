c2925bdb40e012367c895db082e27f7407f61a67
sepagian <rizkydana14@gmail.com>
2025-09-21 22:10:51 +0800

    style(appearance-switcher): format Button component props with multiline syntax

--- src/lib/components/appearance-switcher.svelte
+++ src/lib/components/appearance-switcher.svelte
@@ -3,7 +3,11 @@
 	import { Button } from '$lib/components/ui/button/index.js';
 </script>
 
-<Button onclick={toggleMode} variant="outline" size="icon">
+<Button
+	onclick={toggleMode}
+	variant="outline"
+	size="icon"
+>
 	<div
 		class="i-lucide:sun-medium absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 !transition-all dark:-rotate-90 dark:scale-0"
 	></div>
