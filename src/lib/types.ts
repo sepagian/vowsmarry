f8cfed2908891a15cca086432752e0889481e065
sepagian <rizkydana14@gmail.com>
2025-09-26 00:53:31 +0800

    refactor: update types file

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,23 +4,3 @@ export type Option<T> = {
 	icon?: string;
 	color?: string;
 };
-
-export type Category =
-	| 'accommodation'
-	| 'catering'
-	| 'decoration'
-	| 'entertainment'
-	| 'makeup-attire'
-	| 'paperwork'
-	| 'photo-video'
-	| 'venue'
-	| 'miscellaneous';
-
-export type OverviewCard = {
-	title: string;
-	description: string;
-	action?: string;
-	actionClass?: string;
-	actionColor?: string;
-	footer: string;
-};
