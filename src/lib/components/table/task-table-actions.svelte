17da585e54f610284ae6540ac9f52101c80ae42d
sepagian <rizkydana14@gmail.com>
2025-09-26 18:58:29 +0800

    refactor: update task-table-actions.svelte to display status like priority component

--- src/lib/components/table/task-table-actions.svelte
+++ src/lib/components/table/task-table-actions.svelte
@@ -1,52 +1,29 @@
 <script lang="ts">
-	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index';
 	import { statusOptions } from '$lib/constants/constants';
+	import type { Task } from '$lib/types';
 
 	export let status: Task['status'];
-	export let onChange: (newStatus: Task['status']) => void;
 
-	function getColor(status: Task['status']) {
+	function getStatusData(status: Task['status']) {
 		return (
-			statusOptions.find((s) => s.value === status)?.color ?? 'bg-neutral-100 text-neutral-800'
+			statusOptions.find((s) => s.value === status) ?? {
+				label: '-',
+				color: 'bg-gray-200 text-gray-800',
+				icon: 'i-lucide:minus',
+			}
 		);
 	}
-	function getIcon(status: Task['status']) {
-		return statusOptions.find((s) => s.value === status)?.icon ?? 'i-lucide:alarm-clock-minus';
-	}
-	function getLabel(status: Task['status']) {
-		return statusOptions.find((s) => s.value === status)?.label ?? status;
-	}
+
+	$: statusData = getStatusData(status);
 </script>
 
-<DropdownMenu.Root>
-	<DropdownMenu.Trigger>
-		<div
-			class={`rounded-md px-3 inline-flex items-center gap-2 py-1 text-sm font-medium ${getColor(status)}`}
-		>
-			<div class={`${getIcon(status)}`}></div>
-			{getLabel(status)}
-		</div>
-	</DropdownMenu.Trigger>
-
-	<DropdownMenu.Content class="bg-white">
-		{#each statusOptions as s (s.value)}
-			<DropdownMenu.Item
-				class="flex items-center gap-2"
-				onclick={() => onChange(s.value)}
-			>
-				{#if s.value === status}
-					<div class="i-lucide:check h-4 w-4 text-gray-500"></div>
-				{:else}
-					<span class="h-4 w-4"></span>
-				{/if}
-
-				<span
-					class={`inline-flex rounded-md items-center gap-2 px-2 py-1 text-xs font-medium ${s.color}`}
-				>
-					<div class={`${s.icon}`}></div>
-					{s.label}
-				</span>
-			</DropdownMenu.Item>
-		{/each}
-	</DropdownMenu.Content>
-</DropdownMenu.Root>
+{#if status}
+	<span
+		class="inline-flex items-center rounded-md px-2 py-1 text-xs gap-2 font-medium {statusData.color}"
+	>
+		<div class="{statusData.icon} w-3 h-3"></div>
+		{statusData.label}
+	</span>
+{:else}
+	<span class="text-gray-400">-</span>
+{/if}
