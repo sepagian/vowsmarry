1c9f8d0ed4552dd58092d258f42d65c31036db53
sepagian <rizkydana14@gmail.com>
2025-11-02 20:50:06 +0800

    feat(tasks): Add conditional rendering for wedding profile card

--- src/lib/components/section/section-todo.svelte
+++ src/lib/components/section/section-todo.svelte
@@ -9,23 +9,26 @@
 
 <div class="flex flex-col px-4 gap-4">
 	<div class="flex flex-col gap-4">
-		<Card.Root class="@container/card flex flex-row justify-between items-center shadow-none">
-			<div class="w-full">
-				<Card.Header>
-					<Card.Title class="leading-6 text-base">Almost ready to plan your big day 🎉</Card.Title>
-				</Card.Header>
-				<Card.Content class="text-sm">
-					<p>Complete your wedding profile to see tasks, invitations, and more.</p>
-				</Card.Content>
-			</div>
-			<div class="w-fit justify-end flex px-6">
-				<Button
-					variant="outline"
-					size="icon"
-				>
-					<div class="i-lucide:chevron-right"></div>
-				</Button>
-			</div>
-		</Card.Root>
+		{#if data.wedding}{:else}
+			<Card.Root class="@container/card flex flex-row justify-between items-center shadow-none">
+				<div class="w-full">
+					<Card.Header>
+						<Card.Title class="leading-6 text-base">Almost ready to plan your big day 🎉</Card.Title
+						>
+					</Card.Header>
+					<Card.Content class="text-sm">
+						<p>Complete your wedding profile to see tasks, invitations, and more.</p>
+					</Card.Content>
+				</div>
+				<div class="w-fit justify-end flex px-6">
+					<Button
+						variant="outline"
+						size="icon"
+					>
+						<div class="i-lucide:chevron-right"></div>
+					</Button>
+				</div>
+			</Card.Root>
+		{/if}
 	</div>
 </div>
