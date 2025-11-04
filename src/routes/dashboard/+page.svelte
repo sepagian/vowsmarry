de0a8d8ea021afef6b38a521fe8ec806af226059
sepagian <rizkydana14@gmail.com>
2025-11-04 11:12:25 +0800

    feat(dashboard): Add wedding details edit dialog to dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -2,12 +2,16 @@
 	import SectionCards from '$lib/components/section/section-cards.svelte';
 	import SectionTodo from '$lib/components/section/section-todo.svelte';
 	import SectionBudget from '$lib/components/section/section-budget.svelte';
+	import DialogWedding from '$lib/components/dialog/dialog-wedding.svelte';
+	import * as Dialog from '$lib/components/ui/dialog/index';
+	import { buttonVariants } from '$lib/components/ui/button/index';
 	import { expensesStore } from '$lib/stores/expenses';
 	import { onMount } from 'svelte';
 
 	const overviewTitle = 'Wedding Overview';
 
 	let { data } = $props();
+	let open = $state(false);
 	const weddingDate = data.wedding?.weddingDate ? new Date(data.wedding.weddingDate) : null;
 	const daysUntilWedding = weddingDate
 		? Math.ceil((weddingDate.getTime() - new Date().getTime()) / 86400000)
@@ -55,23 +59,43 @@
 </script>
 
 <div class="flex flex-1 flex-col gap-4 py-4 max-w-screen-xl mx-auto">
-	<div class="flex flex-col gap-2 px-4">
-		<h1 class="text-2xl font-semibold">Welcome back, {data.user.firstName}!</h1>
-		<p class="text-muted-foreground">
-			{#if data.wedding}
-				Plan your wedding with {data.wedding.partnerName || 'your partner'}
-				{#if weddingDate}
-					- {daysUntilWedding} days to go!
+	<div class="flex justify-between px-4 align-center">
+		<div class="flex flex-col gap-2">
+			<h1 class="text-2xl font-semibold">Welcome back, {data.user.firstName}!</h1>
+			<p class="text-muted-foreground">
+				{#if data.wedding}
+					Plan your wedding with {data.wedding.partnerName || 'your partner'}
+					{#if weddingDate}
+						- {daysUntilWedding} days to go!
+					{/if}
+				{:else}
+					Let's start planning your perfect wedding!
 				{/if}
-			{:else}
-				Let's start planning your perfect wedding!
-			{/if}
-		</p>
+			</p>
+		</div>
+		<Dialog.Root bind:open>
+			{#if data.wedding}
+				<Dialog.Trigger
+					class="{buttonVariants({
+						variant: 'default',
+						size: 'default',
+					})} self-center "
+				>
+					<div class="i-lucide:pencil"></div>
+					<span class="hidden lg:inline">Edit wedding details</span>
+				</Dialog.Trigger>
+			{:else}{/if}
+			<DialogWedding
+				{data}
+				bind:open
+			/>
+		</Dialog.Root>
 	</div>
+
+	<SectionTodo {data} />
 	<SectionCards
 		{overviewCards}
 		{overviewTitle}
 	/>
-	<SectionTodo {data} />
 	<SectionBudget {data} />
 </div>
