909dd8ba22102eed7f0f9b8310d172de3b59609d
sepagian <rizkydana14@gmail.com>
2025-11-04 11:10:17 +0800

    feat(tasks): Add task table actions group component

---
+++ src/lib/components/table/task-table-actions-group.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import TaskTableEdit from './task-table-edit.svelte';
+	import TaskTableDelete from './task-table-delete.svelte';
+
+	let { task, data, onUpdate, onDelete } = $props<{
+		task: any;
+		data: any;
+		onUpdate: (taskId: string, updatedData: any) => Promise<void>;
+		onDelete: (taskId: string) => Promise<void>;
+	}>();
+</script>
+
+<div class="flex gap-2">
+	<TaskTableEdit
+		{task}
+		{data}
+		{onUpdate}
+	/>
+	<TaskTableDelete
+		taskId={task.id}
+		taskDescription={task.description}
+		{onDelete}
+	/>
+</div>
