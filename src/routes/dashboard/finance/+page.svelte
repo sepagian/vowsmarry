53fd8313478b09942a9e25537ee5d69a71f29ef5
sepagian <rizkydana14@gmail.com>
2025-11-05 11:53:01 +0800

    feat(finance): Enhance finance dashboard with tabs and improved overview

--- src/routes/dashboard/finance/+page.svelte
+++ src/routes/dashboard/finance/+page.svelte
@@ -1,78 +1,78 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card/index';
+	import * as Tabs from '$lib/components/ui/tabs/index';
 	import SectionCards from '$lib/components/section/section-cards.svelte';
+	import SectionSavings from '$lib/components/section/section-savings.svelte';
 	import ExpenseTable from '$lib/components/table/expense-table.svelte';
 	import ExpenseCategories from '$lib/components/chart/expense-categories.svelte';
-	import ExpenseChart from '$lib/components/chart/expense-chart.svelte';
+	import { onMount } from 'svelte';
+	import { formatDistanceToNow } from 'date-fns';
 
 	import { expensesStore } from '$lib/stores/expenses';
 
 	let { data } = $props();
 
-	let overviewCards = $derived(() => {
-		const expenses = $expensesStore;
-		const totalbudget = 100_000_000;
-		const totalspent = expenses
-			.filter((e) => e['payment-status'] === 'paid')
-			.reduce((sum, e) => sum + e.amount, 0);
-		const pendingSpent = expenses
-			.filter((e) => e['payment-status'] === 'pending')
-			.reduce((sum, e) => sum + e.amount, 0);
-		const remainingbudget = totalbudget - totalspent;
+	onMount(() => {
+		if (data.expenses && data.expenses.length > 0) {
+			expensesStore.set(data.expenses);
+		}
+	});
 
+	let overviewCards = $derived(() => {
 		return [
 			{
-				title: totalbudget.toLocaleString('id-ID', {
+				title: parseFloat(data.stats.plannedBudget).toLocaleString('id-ID', {
 					style: 'currency',
 					currency: 'IDR',
 					minimumFractionDigits: 0,
 					maximumFractionDigits: 0,
 				}),
-				description: 'Total Budget',
-				actionClass: 'i-lucide:book-down',
+				description: 'Planned Budget',
+				actionClass: 'i-lucide:wallet',
 				actionColor: 'bg-green-500 text-white',
-				footer: 'Updated just now',
+				footer: `Last updated ${formatDistanceToNow(new Date(data.update.plannedBudgetUpdate), { addSuffix: true })}`,
 			},
 			{
-				title: totalspent.toLocaleString('id-ID', {
+				title: parseFloat(data.stats.totalSavings).toLocaleString('id-ID', {
 					style: 'currency',
 					currency: 'IDR',
 					minimumFractionDigits: 0,
 					maximumFractionDigits: 0,
 				}),
-				description: 'Budget Spent',
-				actionClass: 'i-lucide:book-up',
+				description: 'Savings',
+				actionClass: 'i-lucide:piggy-bank',
+				actionColor: 'bg-pink-500 text-white',
+				footer: `${data.stats.savingProgress}% saved`,
+			},
+			{
+				title: parseFloat(data.stats.budgetSpent).toLocaleString('id-ID', {
+					style: 'currency',
+					currency: 'IDR',
+					minimumFractionDigits: 0,
+					maximumFractionDigits: 0,
+				}),
+				description: 'Expenses',
+				actionClass: 'i-lucide:receipt-text',
 				actionColor: 'bg-yellow-500 text-white',
-				footer: 'Updated just now',
+				footer: `Last updated ${formatDistanceToNow(new Date(data.update.budgetSpentUpdate), { addSuffix: true })}`,
 			},
+
 			{
-				title: pendingSpent.toLocaleString('id-ID', {
+				title: parseFloat(data.stats.budgetRemaining).toLocaleString('id-ID', {
 					style: 'currency',
 					currency: 'IDR',
 					minimumFractionDigits: 0,
 					maximumFractionDigits: 0,
 				}),
-				description: 'Pending Payment',
-				actionClass: 'i-lucide:book-x',
-				actionColor: 'bg-gray-500 text-white',
-				footer: 'Updated just now',
-			},
-			{
-				title: remainingbudget.toLocaleString('id-ID', {
-					style: 'currency',
-					currency: 'IDR',
-					minimumFractionDigits: 0,
-					maximumFractionDigits: 0,
-				}),
-				description: 'Budget Remaining',
-				actionClass: 'i-lucide:book-text',
+				description: 'Remaining Balance',
+				actionClass: 'i-lucide:chart-area',
 				actionColor: 'bg-blue-500 text-white',
-				footer: 'Updated just now',
+				footer: `Last updated ${formatDistanceToNow(new Date(data.update.budgetSpentUpdate), { addSuffix: true })}`,
 			},
 		];
 	});
 
-	const overviewTitle = 'Budget Overview';
+	const overviewTitle = 'Finances Overview';
 </script>
 
 <div class="flex flex-1 flex-col gap-2 py-4 max-w-screen-xl mx-auto">
@@ -80,19 +80,33 @@
 		{overviewCards}
 		{overviewTitle}
 	/>
-	<div class="flex px-4">
-		<ExpenseChart />
-	</div>
-	<div class="grid grid-cols-3 gap-4 flex flex-col px-4">
-		<div class="flex flex-col col-span-3 lg:col-span-2 gap-2">
-			<ExpenseTable {data} />
-		</div>
-		<div class="flex flex-col col-span-3 lg:col-span-1 row-span-3 gap-2 pb-4">
-			<Card.Root class="@container/card p-6 h-full gap-2 shadow-none">
-				<Card.Content class="p-0">
-					<ExpenseCategories />
-				</Card.Content>
-			</Card.Root>
-		</div>
-	</div>
+	<Tabs.Root
+		value="expense"
+		class="px-4"
+	>
+		<Tabs.List class="w-full">
+			<Tabs.Trigger value="expense">Expenses</Tabs.Trigger>
+			<Tabs.Trigger value="savings">Savings</Tabs.Trigger>
+		</Tabs.List>
+		<Tabs.Content value="expense">
+			<div class="grid grid-cols-3 gap-4 flex flex-col">
+				<div class="flex flex-col col-span-3 lg:col-span-2 gap-2">
+					<ExpenseTable
+						{data}
+						allowAdd={true}
+					/>
+				</div>
+				<div class="flex flex-col col-span-3 lg:col-span-1 row-span-3 gap-2 pb-4">
+					<Card.Root class="@container/card p-6 h-full gap-2 shadow-none">
+						<Card.Content class="p-0">
+							<ExpenseCategories />
+						</Card.Content>
+					</Card.Root>
+				</div>
+			</div>
+		</Tabs.Content>
+		<Tabs.Content value="savings">
+			<SectionSavings />
+		</Tabs.Content>
+	</Tabs.Root>
 </div>
