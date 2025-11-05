e95f0872bdfaceb2a4457dc6fa1e606b6d10d0b6
sepagian <rizkydana14@gmail.com>
2025-11-05 11:46:08 +0800

    style(section): Refine budget section layout and expense table

--- src/lib/components/section/section-budget.svelte
+++ src/lib/components/section/section-budget.svelte
@@ -4,14 +4,17 @@
 	let { data } = $props();
 </script>
 
-<div class="flex flex-col gap-4 px-4">
+<div class="flex flex-col gap-2 px-4">
 	<div class="flex flex-col">
 		<h2 class="text-base font-bold text-neutral-600">Recent Expenses</h2>
 	</div>
 
 	<div class="flex flex-col gap-4">
 		<div class="flex flex-col">
-			<ExpenseTable {data} />
+			<ExpenseTable
+				{data}
+				allowAdd={false}
+			/>
 		</div>
 	</div>
 </div>
