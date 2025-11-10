0211f17fc4a5d001b02638655ebc155c6dcdf939
sepagian <rizkydana14@gmail.com>
2025-11-10 11:52:23 +0800

    refactor(sidebar): Update navigation menu item label and route

--- src/lib/components/sidebar/app-sidebar.svelte
+++ src/lib/components/sidebar/app-sidebar.svelte
@@ -8,7 +8,7 @@
 		{ title: 'Documents', url: '/dashboard/document', icon: 'i-lucide:file-text' }, // DOCUMENTS & PAPERWORK
 		{ title: 'Finance', url: '/dashboard/finance', icon: 'i-lucide:dollar-sign' }, // EXPENSES AND SAVINGS
 		{ title: 'Vendors', url: '/dashboard/vendor', icon: 'i-lucide:signature' }, // VENDORS
-		{ title: 'Rundown', url: '/dashboard/rundown', icon: 'i-lucide:calendar-1' }, // RUNDOWN & DRESSCODES
+		{ title: 'Schedules', url: '/dashboard/schedules', icon: 'i-lucide:calendar-1' }, // RUNDOWN & DRESSCODES
 		{ title: 'Setup', url: '/dashboard/invitation', icon: 'i-lucide:settings' }, // OVERVIEW, TEMPLATES, COUPLES DETAILS
 		{ title: 'Story', url: '/dashboard/invitation/story', icon: 'i-lucide:notebook-pen' }, // LOVE STORY & GALLERY
 		{ title: 'Guests', url: '/dashboard/invitation/guest', icon: 'i-lucide:users' }, // RSVPS
