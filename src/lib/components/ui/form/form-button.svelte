e99abe4b82bfbb5cfffe2560bf4a4c0195e8daa8
sepagian <rizkydana14@gmail.com>
2025-11-18 14:07:05 +0800

    refactor(form): Update form button type annotation for clarity

--- src/lib/components/ui/form/form-button.svelte
+++ src/lib/components/ui/form/form-button.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import * as Button from '$lib/components/ui/button/index.js';
 
-	let { ref = $bindable(null), ...restProps }: Button.Props = $props();
+	let { ref = $bindable(null), ...restProps }: Button.ButtonElementProps = $props();
 </script>
 
 <Button.Root
