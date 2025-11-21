c922f8f6bbb56409224b7963ea82d93303b6c699
sepagian <rizkydana14@gmail.com>
2025-11-22 02:37:29 +0800

    chore(db): Migrate Drizzle configuration from PostgreSQL to Cloudflare D1

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,14 @@
+import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-
 export default defineConfig({
-	schema: './src/lib/server/db/schema/',
 	out: './drizzle',
-	dialect: 'postgresql',
-	dbCredentials: { url: process.env.DATABASE_URL },
-	verbose: true,
-	strict: true,
+	schema: './src/lib/server/db/schema/*',
+	dialect: 'sqlite',
+	driver: 'd1-http',
+	dbCredentials: {
+		accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+		databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
+		token: process.env.CLOUDFLARE_D1_TOKEN!,
+	},
 });
