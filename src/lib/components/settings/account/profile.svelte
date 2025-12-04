6fe9ff96411e9df951468a7438948d42a4242c9a
sepagian <rizkydana14@gmail.com>
2025-12-04 16:38:32 +0800

    feat: add profile settings component with sections

---
+++ src/lib/components/settings/account/profile.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import ProfileSection from '$lib/components/settings/account/profile/profile-section.svelte';
+	import PasswordSection from '$lib/components/settings/account/profile/password-section.svelte';
+	import TwofaSection from '$lib/components/settings/account/profile/twofa-section.svelte';
+	import ActiveSection from '$lib/components/settings/account/profile/active-section.svelte';
+</script>
+
+<div class="flex flex-col gap-4">
+	<ProfileSection />
+	<PasswordSection />
+	<TwofaSection />
+	<ActiveSection />
+</div>
