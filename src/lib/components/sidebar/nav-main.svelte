a1150f84324a439863ec1c47b82e324d1e8d0215
sepagian <rizkydana14@gmail.com>
2025-12-04 16:39:00 +0800

    feat: update main navigation

--- src/lib/components/sidebar/nav-main.svelte
+++ src/lib/components/sidebar/nav-main.svelte
@@ -1,25 +1,18 @@
 <script lang="ts">
 	import { active } from '$lib/actions/active.svelte';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import type { WithoutChildren } from '$lib/utils.js';
+	import type { ComponentProps } from 'svelte';
 
 	let {
 		items,
-	}: {
-		items: {
-			title: string;
-			url?: string;
-			icon?: string;
-			isActive?: boolean;
-			items?: {
-				title: string;
-				url: string;
-				icon?: string;
-			}[];
-		}[];
-	} = $props();
+		...restProps
+	}: { items: { title: string; url: string; icon: string }[] } & WithoutChildren<
+		ComponentProps<typeof Sidebar.Group>
+	> = $props();
 </script>
 
-<Sidebar.Group class="border-t-1">
+<Sidebar.Group class="border-t-1" {...restProps}>
 	<Sidebar.Menu class="gap-1">
 		{#each items as item (item.title)}
 			<Sidebar.MenuItem>
