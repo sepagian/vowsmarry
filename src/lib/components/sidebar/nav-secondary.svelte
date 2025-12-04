4c88f829e02484ec49078222f84d9d1293b5a890
sepagian <rizkydana14@gmail.com>
2025-12-04 16:38:38 +0800

    feat: add secondary navigation component

---
+++ src/lib/components/sidebar/nav-secondary.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import type { WithoutChildren } from '$lib/utils.js';
+	import type { ComponentProps } from 'svelte';
+	let {
+		items,
+		...restProps
+	}: { items: { title: string; url: string; icon: string }[] } & WithoutChildren<
+		ComponentProps<typeof Sidebar.Group>
+	> = $props();
+</script>
+
+<Sidebar.Group {...restProps}>
+	<Sidebar.GroupContent>
+		<Sidebar.Menu>
+			{#each items as item (item.title)}
+				<Sidebar.MenuItem>
+					<Sidebar.MenuButton>
+						{#snippet child({ props })}
+							<a
+								href={item.url}
+								{...props}
+							>
+								<div class="{item.icon} h-4 w-4"></div>
+								<span>{item.title}</span>
+							</a>
+						{/snippet}
+					</Sidebar.MenuButton>
+				</Sidebar.MenuItem>
+			{/each}
+		</Sidebar.Menu>
+	</Sidebar.GroupContent>
+</Sidebar.Group>
