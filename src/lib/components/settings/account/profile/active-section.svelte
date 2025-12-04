7143cd23eabfc1d7b71980b15770fe966f4225b0
sepagian <rizkydana14@gmail.com>
2025-12-04 16:38:32 +0800

    feat: add active sessions management section

---
+++ src/lib/components/settings/account/profile/active-section.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Separator } from '$lib/components/ui/separator/index';
+	import { Button } from '$lib/components/ui/button/index';
+</script>
+
+<div class="flex flex-row items-center justify-between">
+	<div class="flex flex-col">
+		<h1 class="font-extrabold text-xl">Active Sessions</h1>
+		<span class="text-muted-foreground text-sm text-balance"
+			>Review recent devices and sign out of anything you don’t recognize.</span
+		>
+	</div>
+	<Button>View Sessions</Button>
+</div>
+<Separator />
