8dfa3cf6ba4f608c6fcdb1e8ff116d02f4081476
sepagian <rizkydana14@gmail.com>
2025-12-15 21:41:38 +0800

    refactor(onboarding): update imports to named imports

--- src/routes/onboarding/step-three.svelte
+++ src/routes/onboarding/step-three.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button';
-	import * as FieldSet from '$lib/components/ui/field-set';
-	import WorkspaceForm from '$lib/components/form/workspace-form.svelte';
 	import { toast } from 'svelte-sonner';
+
+	import WorkspaceForm from '$lib/components/form/workspace-form.svelte';
+	import { Button } from '$lib/components/ui/button';
+	import { Content as FieldSetContent, Footer as FieldSetFooter, Root as FieldSetRoot, Title as FieldSetTitle } from '$lib/components/ui/field-set';
 
 	let { handleBack, handleNext, wizardFormState } = $props();
 
@@ -21,23 +22,23 @@
 </script>
 
 <div class="w-full h-xl pt-4">
-	<FieldSet.Root class=" justify-between rounded-xl overflow-hidden">
+	<FieldSetRoot class=" justify-between rounded-xl overflow-hidden">
 		<div class="flex flex-col">
-			<FieldSet.Title class="p-6 font-bold flex flex-col gap-1">
+			<FieldSetTitle class="p-6 font-bold flex flex-col gap-1">
 				<h1 class="font-semibold text-lg">Name your wedding space</h1>
 				<span class="text-sm text-balance text-muted-foreground">
 					This is the home for every plan, task, and memory you'll build together. A name makes it
 					feel real, a little moment to smile at.
 				</span>
-			</FieldSet.Title>
-			<FieldSet.Content class="p-6 text-balance text-center text-foreground">
+			</FieldSetTitle>
+			<FieldSetContent class="p-6 text-balance text-center text-foreground">
 				<WorkspaceForm
 					data={wizardFormState}
 					onSuccess={handleSuccess}
 				/>
-			</FieldSet.Content>
+			</FieldSetContent>
 		</div>
-		<FieldSet.Footer class="flex px-4 py-6 justify-between">
+		<FieldSetFooter class="flex px-4 py-6 justify-between">
 			<Button
 				onclick={handleBack}
 				size="sm"
@@ -50,6 +51,6 @@
 				size="sm"
 				class="bg-orange-200 text-orange-800 hover:bg-orange-300">Next</Button
 			>
-		</FieldSet.Footer>
-	</FieldSet.Root>
+		</FieldSetFooter>
+	</FieldSetRoot>
 </div>
