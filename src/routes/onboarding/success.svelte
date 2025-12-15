711deefa90fc2c8a3e2355940dafe7ce9a58255f
sepagian <rizkydana14@gmail.com>
2025-12-15 21:41:38 +0800

    refactor(onboarding): update imports to named imports

--- src/routes/onboarding/success.svelte
+++ src/routes/onboarding/success.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-	import * as Empty from '$lib/components/ui/empty/index.js';
-	import { Button } from '$lib/components/ui/button/index.js';
+
+	import { Empty, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from '$lib/components/ui/empty/index.js';
 </script>
 
-<Empty.Root>
-	<Empty.Header>
-		<Empty.Media
+<Empty>
+	<EmptyHeader>
+		<EmptyMedia
 			variant="icon"
 			class="bg-orange-200"
 		>
 			<div class="i-lucide:check bg-orange-800"></div>
-		</Empty.Media>
-		<Empty.Title class="font-semibold text-lg">You’re all set</Empty.Title>
-		<Empty.Description class="text-sm text-muted-foreground">
-			Everything’s ready. Step in and start planning the things that matter, one moment at a time.
-		</Empty.Description>
-	</Empty.Header>
-</Empty.Root>
+		</EmptyMedia>
+		<EmptyTitle class="font-semibold text-lg">You're all set</EmptyTitle>
+		<EmptyDescription class="text-sm text-muted-foreground">
+			Everything's ready. Step in and start planning the things that matter, one moment at a time.
+		</EmptyDescription>
+	</EmptyHeader>
+</Empty>
