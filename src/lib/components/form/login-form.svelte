ea58d4f000c40319e059dddf29121c5e76b937fe
sepagian <rizkydana14@gmail.com>
2025-12-15 21:41:27 +0800

    refactor(form): update imports to named imports

--- src/lib/components/form/login-form.svelte
+++ src/lib/components/form/login-form.svelte
@@ -3,9 +3,9 @@
 	import { valibot } from "sveltekit-superforms/adapters";
 
 	import { Button } from "$lib/components/ui/button/index";
-	import * as Form from "$lib/components/ui/form/index";
+	import { FormControl, FormField, FormFieldErrors, FormLabel } from "$lib/components/ui/form/index";
 	import { Input } from "$lib/components/ui/input/index";
-	import * as Password from "$lib/components/ui/password/index";
+	import { Input as PasswordInput, Root as PasswordRoot, ToggleVisibility } from "$lib/components/ui/password/index";
 
 	import { loginSchema } from "$lib/validation/auth";
 
@@ -28,17 +28,17 @@
 <div class="flex flex-col gap-8">
 	<div class="flex flex-col items-center gap-2 text-center">
 		<h1 class="text-2xl font-bold">
-			Welcome back! Let’s get you back to planning something beautiful
+			Welcome back! Let's get you back to planning something beautiful
 		</h1>
 		<p class="text-muted-foreground text-balance text-sm">
 			Log in to continue where you left off — your journey together awaits.
 		</p>
 	</div>
 	<form method="POST" class="flex flex-col gap-2" use:enhance>
-		<Form.Field {form} name="email">
-			<Form.Control>
+		<FormField {form} name="email">
+			<FormControl>
 				{#snippet children({ props })}
-					<Form.Label>Email</Form.Label>
+					<FormLabel>Email</FormLabel>
 					<Input
 						{...props}
 						type="email"
@@ -46,31 +46,31 @@
 						bind:value={$formData.email}
 					/>
 				{/snippet}
-			</Form.Control>
-			<Form.FieldErrors class="text-xs text-red-500"/>
-		</Form.Field>
-		<Form.Field {form} name="password">
-			<Form.Control>
+			</FormControl>
+			<FormFieldErrors class="text-xs text-red-500"/>
+		</FormField>
+		<FormField {form} name="password">
+			<FormControl>
 				{#snippet children({ props })}
-					<Form.Label>Password</Form.Label>
-					<Password.Root>
-						<Password.Input
+					<FormLabel>Password</FormLabel>
+					<PasswordRoot>
+						<PasswordInput
 							{...props}
 							bind:value={$formData.password}
 							placeholder="Enter your password"
 						>
-							<Password.ToggleVisibility />
-						</Password.Input>
-					</Password.Root>
+							<ToggleVisibility />
+						</PasswordInput>
+					</PasswordRoot>
 				{/snippet}
-			</Form.Control>
+			</FormControl>
 			<div class="flex flex-1 justify-between">
-				<Form.FieldErrors class="text-xs text-red-500"/>
+				<FormFieldErrors class="text-xs text-red-500"/>
 				<a href="/forgot-password" class="ml-auto text-xs hover:underline">
 					Forgot your password?
 				</a>
 			</div>
-		</Form.Field>
+		</FormField>
 		<Button type="submit" variant="outline" class="w-full cursor-pointer">
 			Login
 		</Button>
