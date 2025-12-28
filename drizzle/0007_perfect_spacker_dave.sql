622fdcfc9ee7719f1187fcaba2a5b6b4fa7c3ae3
Rizky Dana <rizkydana14@gmail.com>
2025-12-28 13:43:55 +0800

    fix(schema): remove old drizzle migrations

--- drizzle/0007_perfect_spacker_dave.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `organization` ADD `groom_name` text;--> statement-breakpoint
-ALTER TABLE `organization` ADD `bride_name` text;--> statement-breakpoint
-ALTER TABLE `organization` ADD `wedding_date` text;--> statement-breakpoint
-ALTER TABLE `organization` ADD `wedding_venue` text;--> statement-breakpoint
-ALTER TABLE `organization` ADD `wedding_budget` text;
