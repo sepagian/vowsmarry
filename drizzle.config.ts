f52c21267e2e358590f039b06d45fbfe9c6ed30f
Rizky Dana <rizkydana14@gmail.com>
2025-12-28 13:44:58 +0800

    lint: normalize string quotes in drizzle.config and server.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-	out: './drizzle',
-	schema: './src/lib/server/db/schema/*',
-	dialect: 'sqlite',
-	driver: 'd1-http',
+	out: "./drizzle",
+	schema: "./src/lib/server/db/schema/*",
+	dialect: "sqlite",
+	driver: "d1-http",
 	dbCredentials: {
 		accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
 		databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
